517ee85bfe474923eeb1ffd6212c3ffc8dcb0783
MaxAlbero <maximo.albero@enti.cat>
2025-09-22 17:44:01 +0200

    Finalitzat tutorial Threads

--- SistemesOperatius/Sistemes/1TutorialThreads/ThreadTutorial.h
+++ SistemesOperatius/Sistemes/1TutorialThreads/ThreadTutorial.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <thread>
 #include <chrono> //time
+#include <list>
 
 #pragma region Functions Declaration Header
 
@@ -32,35 +33,35 @@ void ThreadTutorialTest() {
 
 	startDate = std::chrono::system_clock::now();
 
-	int exampleUsed = 1; //Change this variable for the use of different examples
+	int exampleUsed = 7; //Change this variable for the use of different examples
 
 	switch (exampleUsed) {
 	case 1:
 		Example01();
 		break;
 	case 2:
-		Example01();
+		Example02();
 		break;
 	case 3:
-		Example01();
+		Example03();
 		break;
 	case 4:
-		Example01();
+		Example04();
 		break;
 	case 5:
-		Example01();
+		Example05();
 		break;
 	case 6:
-		Example01();
+		Example06();
 		break;
 	case 7:
-		Example01();
+		Example07();
 		break;
 	default:
 		break;
 	}
 
-	std::cout << "Example 0 " << exampleUsed << " finished" << std::endl;
+	std::cout << "Example 0" << exampleUsed << " finished" << std::endl;
 	std::string text = "";
 	std::getline(std::cin, text);
 }
@@ -89,6 +90,7 @@ void Example01() {
 	std::thread* Counter2 = new std::thread(Count, totalCount, "2");
 	std::thread* Counter3 = new std::thread(Count, totalCount, "3");
 
+	//join() ens serveix per a fer que els threads s'esperin als altres per executar-se
 	Counter1->join();
 	Counter2->join();
 	Counter3->join();
@@ -96,20 +98,115 @@ void Example01() {
 	Count(totalCount, "0");
 }
 void Example02() {
+	unsigned long long totalCount = 1000000000ull;
 
+	//num threads creats: 1
+	std::thread* Counter1 = new std::thread(Count, totalCount, "1");
+	
+	Counter1->join();
+	//num threads executats: 1
+	//num threads creats->join: 1
+
+	//num threads creats: 3
+	std::thread* Counter2 = new std::thread(Count, totalCount, "2");
+	std::thread* Counter3 = new std::thread(Count, totalCount, "3");
+
+	Counter2->join();
+	Counter3->join();
+	//num threads executats: 3
+	//num threads creats->join: 3
+
+	Count(totalCount, "0");
 }
 void Example03() {
+	//en el moment d'executar el programa, el nostre thread principal 
+	//perd el control sobre les dades
+	unsigned long long totalCount = 1000000000ull;
 
+	std::thread* Counter1 = new std::thread(Count, totalCount, "1");
+	std::thread* Counter2 = new std::thread(Count, totalCount, "2");
+	std::thread* Counter3 = new std::thread(Count, totalCount, "3");
+
+
+	Counter1->detach();
+	Counter2->detach();
+	Counter3->detach();
+
+	Count(totalCount, "0");
 }
 void Example04() {
+	std::list<std::thread*>* threads = new std::list<std::thread*>();
 
+	unsigned long long totalCount = 1000000000ull;
+
+	for (int i = 0; i < 3; i++) {
+		std::thread* counterThread = new std::thread(Count, totalCount, std::to_string(i + 1));
+		threads->push_back(counterThread);
+		counterThread->join();
+	}
+
+	Count(totalCount, "0");
 }
 void Example05() {
+	std::list<std::thread*>* threads = new std::list<std::thread*>();
 
+	unsigned long long totalCount = 1000000000ull;
+
+	for (int i = 0; i < 3; i++) {
+		std::thread* counterThread = new std::thread(Count, totalCount, std::to_string(i + 1));
+		threads->push_back(counterThread);
+		counterThread->detach();
+	}
+
+	Count(totalCount, "0");
 }
 void Example06() {
+	unsigned int maxThreads = std::thread::hardware_concurrency();
+	std::cout << maxThreads << " concurrent threads are supported" << std::endl;
 
+	int extraThreads = 0;
+
+	unsigned long long totalCount = 1000000000ull;
+
+	unsigned int totalThreads = maxThreads + extraThreads;
+
+	std::list<std::thread*>* threads = new std::list<std::thread*>();
+
+	for (int i = 0; i < totalThreads; i++)
+	{
+		std::thread* counterThread = new std::thread(Count, totalCount, std::to_string(i + 1));
+		threads->push_back(counterThread);
+	}
+
+	for (std::thread* t : *threads)
+	{
+		t->join();
+	}
+
+	Count(totalCount, "0");
 }
-void Example07() {
 
+class ThreadTester {
+public:
+	ThreadTester() {}
+	~ThreadTester() {}
+
+	void Example07();
+
+private:
+	void TestFunction(unsigned long long testInt);
+};
+
+void ThreadTester::Example07() {
+	std::thread* testThread = new std::thread(&ThreadTester::TestFunction, this, 14ull);
+	testThread->join();
+}
+
+void ThreadTester::TestFunction(unsigned long long testInt) {
+	std::cout << "Number is: " << testInt << std::endl;
+}
+
+void Example07() {
+	ThreadTester* threadTester = new ThreadTester();
+	threadTester->Example07();
 }
